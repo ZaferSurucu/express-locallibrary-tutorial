b942ffef564663d9c690df7331c07385c4ace3ea
ZaferSurucu <scream_boy.s@hotmail.com>
2023-04-12 01:08:29 +0300

    changed mongodb url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var limiter = RateLimit({
 
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
-const dev_db_url = "mongodb+srv://zafer:3578520@cluster0.aynz0gw.mongodb.net/?retryWrites=true&w=majority"
+const dev_db_url = "mongodb+srv://<username>:<password>@cluster0.aynz0gw.mongodb.net/?retryWrites=true&w=majority"
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch(err => console.log(err));
